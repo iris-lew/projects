3e99e30231da06681cbfdd493cd56645a5bb4e9b
Iris Lew <iris.lew@berkeley.edu>
2023-02-12 11:08:58 -0800

    Added landing page and images (possible usage)

---
+++ main.js
@@ -0,0 +1,16 @@
+projects_grid = document.querySelector('#projects_grid_id');
+cv = document.querySelector('#cv_describe_id');
+isClicked=true;
+
+function toggle_func() {
+    if (isClicked){
+        projects_grid.style.display="None";
+        cv.style.display="block";
+        isClicked = false;
+    } else{
+        projects_grid.style.display="inline-grid";
+        cv.style.display="None";
+        isClicked = true;
+    }
+    
+  }
