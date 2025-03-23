f50c4ede57074ea69259398261161e99d0bad9bc
Iris Lew <iris.lew@berkeley.edu>
2025-03-22 17:18:00 -0700

    Actually adding the updates to the portfolio

--- main.js
+++ main.js
@@ -1,39 +1,37 @@
-
 projects_grid = document.querySelector('#projects_grid_id');
 cv = document.querySelector('#cv_describe_id');
 isClicked=true;
 
-// function toggle_func() {
-//     if (isClicked){
-//         projects_grid.style.display="None";
-//         cv.style.display="block";
-//         isClicked = false;
-//     } else{
-//         projects_grid.style.display="inline-grid";
-//         cv.style.display="None";
-//         isClicked = true;
-//     }
+function toggle_func() {
+    if (isClicked){
+        projects_grid.style.display="None";
+        cv.style.display="block";
+        isClicked = false;
+    } else{
+        projects_grid.style.display="inline-grid";
+        cv.style.display="None";
+        isClicked = true;
+    }
     
-//   }
-
-function openContent(evt, ContentName) {
-    // Declare all variables
-    var i, tabcontent, tablinks;
-  
-    // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-  
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-  
-    // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(ContentName).style.display = "block";
-    evt.currentTarget.className += " active";
   }
-document.getElementById("defaultOpen").click();
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("Taskmaster");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+} 
